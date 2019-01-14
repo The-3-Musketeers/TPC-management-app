d11ab5c07152a2d83505d18e13a2c7e06b10e60e
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-01-15 00:33:27 +0530

    Add Student Login and Navbar

---
+++ navbar.php
@@ -0,0 +1,49 @@
+<?php
+echo '<div class="container">
+        <div class="page-header">
+            <div style="display: flex;">
+                <a href="./index.html"><img src="./iitp_logo.png" height="80"></a>
+                <div style="padding-bottom: 5px; padding-left: 20px;">
+                <h3>Training &amp; Placement Cell</h3>
+                <h5><strong>Indian Institute of Technology Patna</strong></h5>
+                </div>
+            </div>
+        </div>
+    </div>
+<div class="container">
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        </button>
+    </div>
+ 
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="./index.php">Home</a></li>
+        <li><a href="#">Why IIT Patna?</a></li>
+        <li><a href="#">Departments</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contact Us <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Email</a></li>
+            <li><a href="#">Phone</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Set Appointment</a></li>
+          </ul>
+        </li>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Recruiter Login</a></li>
+            <li><a href="./studentLogin.php">Student Login</a></li>
+        </ul>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</div>';
+?>
