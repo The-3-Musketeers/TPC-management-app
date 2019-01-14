97bfadb2b733d386d66230134c818538dd014fe3
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-01-15 00:33:27 +0530

    Add Student Login and Navbar

---
+++ studentLogin.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- If IE use the latest rendering engine -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Set the page to the width of the device and set the zoon level -->
+    <meta name="viewport" content="width = device-width, initial-scale = 1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <title>Home - T&amp;P IIT Patna</title>
+</head>
+<body>
+    <?php include 'navbar.php';?>
+    <br>
+
+    <script>
+    
+    </script>
+    
+    <form class="form-horizontal" action="">
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="email">Roll Number:</label>
+            <div class="col-sm-6">
+            <input type="email" class="form-control" id="email" placeholder="Enter roll number">
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="email">Full Name:</label>
+            <div class="col-sm-6">
+            <input type="email" class="form-control" id="email" placeholder="Enter full name">
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="email">Webmail ID:</label>
+            <div class="col-sm-6">
+            <input type="email" class="form-control" id="email" placeholder="Enter webmail id">
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="pwd">Password:</label>
+            <div class="col-sm-6"> 
+            <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="pwd">Comfirm Password:</label>
+            <div class="col-sm-6"> 
+            <input type="password" class="form-control" id="pwd" placeholder="Re-Enter password">
+            </div>
+        </div>
+        <div class="form-group"> 
+            <div class="col-sm-offset-2 col-sm-10">
+            <div class="checkbox">
+                <label><input type="checkbox"> Remember me</label>
+            </div>
+            </div>
+        </div>
+        <div class="form-group"> 
+            <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-default">Submit</button>
+            </div>
+        </div>
+    </form>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
