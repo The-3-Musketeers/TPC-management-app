503b4176e2f6468b3e62896b0fc2c266e00a1ab3
PRP <prp9812@gmail.com>
2019-01-15 11:43:43 +0530

    add components as templates

--- studentLogin.php
+++ studentLogin.php
@@ -1,27 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- If IE use the latest rendering engine -->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Set the page to the width of the device and set the zoon level -->
-    <meta name="viewport" content="width = device-width, initial-scale = 1">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
-    <title>Home - T&amp;P IIT Patna</title>
-</head>
-<body>
-    <?php include 'navbar.php';?>
+<?php
+    $page_title = 'Login - T&amp;P IIT Patna';
+    include 'templates/header.php';
+    include 'templates/navbar.php';?>
     <br>
 
     <script>
-    
+
     </script>
-    
+
     <form class="form-horizontal" action="">
         <div class="form-group">
             <label class="control-label col-sm-2" for="email">Roll Number:</label>
@@ -43,32 +29,27 @@
         </div>
         <div class="form-group">
             <label class="control-label col-sm-2" for="pwd">Password:</label>
-            <div class="col-sm-6"> 
+            <div class="col-sm-6">
             <input type="password" class="form-control" id="pwd" placeholder="Enter password">
             </div>
         </div>
         <div class="form-group">
             <label class="control-label col-sm-2" for="pwd">Comfirm Password:</label>
-            <div class="col-sm-6"> 
+            <div class="col-sm-6">
             <input type="password" class="form-control" id="pwd" placeholder="Re-Enter password">
             </div>
         </div>
-        <div class="form-group"> 
+        <div class="form-group">
             <div class="col-sm-offset-2 col-sm-10">
             <div class="checkbox">
                 <label><input type="checkbox"> Remember me</label>
             </div>
             </div>
         </div>
-        <div class="form-group"> 
+        <div class="form-group">
             <div class="col-sm-offset-2 col-sm-10">
             <button type="submit" class="btn btn-default">Submit</button>
             </div>
         </div>
     </form>
-
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-</body>
-</html>
+<?php include 'templates/footer.php';?>
