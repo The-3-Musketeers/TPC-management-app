74eed57b18f801a03d8018320c6832054a458810
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-02-10 01:13:32 +0530

    Add accept and reject of company functionality

--- admin/viewCompanies.php
+++ admin/viewCompanies.php
@@ -2,31 +2,70 @@
   // Start the session
   require_once('../templates/startSession.php');
   require_once('../connectVars.php');
-
   // Authenticate user
   require_once('../templates/auth.php');
   checkUserRole('admin', $auth_error);
-
   $page_title = 'Company';
   require_once('../templates/header.php');
   require_once('../templates/navbar.php');
 
+  $activeTab = "1";
+  if(isset($_POST['approve'])){
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    if (!$dbc) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+    $company_id = mysqli_real_escape_string($dbc, trim($_GET['id']));
+    $update_status_query = "UPDATE recruiters SET company_status='accepted' WHERE company_id='$company_id'";
+    $update_status = mysqli_query($dbc, $update_status_query);
+    if(!$update_status){
+      echo '<div class="container"><div class="alert alert-warning alert-dismissible fade show" role="alert">' .
+        'Failed to update. Please try again.' . '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' .
+        '<span aria-hidden="true">&times;</span></button></div></div>';
+      die("QUERY FAILED ".mysqli_error($dbc));
+    } else {
+      echo '<div class="container"><div class="alert alert-success alert-dismissible fade show" role="alert">' .
+          'Successfully Updated.<button type="button" class="close" data-dismiss="alert" aria-label="Close">' .
+          '<span aria-hidden="true">&times;</span></button></div></div>';
+    }
+    $activeTab = $_GET['tab'];
+  }
+  if(isset($_POST['reject'])){
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    if (!$dbc) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+    $company_id = mysqli_real_escape_string($dbc, trim($_GET['id']));
+    $update_status_query = "UPDATE recruiters SET company_status='rejected' WHERE company_id='$company_id'";
+    $update_status = mysqli_query($dbc, $update_status_query);
+    if(!$update_status){
+      echo '<div class="container"><div class="alert alert-warning alert-dismissible fade show" role="alert">' .
+        'Failed to update. Please try again.' . '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' .
+        '<span aria-hidden="true">&times;</span></button></div></div>';
+      die("QUERY FAILED ".mysqli_error($dbc));
+    } else {
+      echo '<div class="container"><div class="alert alert-success alert-dismissible fade show" role="alert">' .
+          'Successfully Updated.<button type="button" class="close" data-dismiss="alert" aria-label="Close">' .
+          '<span aria-hidden="true">&times;</span></button></div></div>';
+    }
+    $activeTab = $_GET['tab'];
+  }
 ?>
 
 <div class="container">
   <ul class="nav nav-tabs" id="companiesTab" role="tablist">
     <li class="nav-item">
-      <a class="nav-link active" id="home-tab" data-toggle="tab" href="#accepted" role="tab" aria-controls="home" aria-selected="true">Accepted</a>
+      <a class="nav-link <?php if($activeTab==1){echo 'active';} ?>" id="home-tab" data-toggle="tab" href="#accepted" role="tab" aria-controls="home" aria-selected="true">Accepted</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" id="profile-tab" data-toggle="tab" href="#pending" role="tab" aria-controls="profile" aria-selected="false">Pending</a>
+      <a class="nav-link <?php if($activeTab==2){echo 'active';} ?>" id="profile-tab" data-toggle="tab" href="#pending" role="tab" aria-controls="profile" aria-selected="false">Pending</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" id="contact-tab" data-toggle="tab" href="#rejected" role="tab" aria-controls="contact" aria-selected="false">Rejected</a>
+      <a class="nav-link <?php if($activeTab==3){echo 'active';} ?>" id="contact-tab" data-toggle="tab" href="#rejected" role="tab" aria-controls="contact" aria-selected="false">Rejected</a>
     </li>
   </ul>
   <div class="tab-content" id="companiesTabContent">
-    <div class="tab-pane fade show active" id="accepted">
+    <div class="tab-pane fade <?php if($activeTab==1){echo 'show active';} ?>" id="accepted">
       <table class="table">
         <thead class="thead-light">
           <tr>
@@ -35,11 +74,12 @@
             <th scope="col">Category</th>
             <th scope="col">HR Name</th>
             <th scope="col">Email</th>
+            <th scope="col">Action</th>
           </tr>
         </thead>
         <?php
           $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-          $query = "SELECT company_name, company_category, hr_name, hr_email FROM recruiters WHERE company_status='accepted'";
+          $query = "SELECT company_id, company_name, company_category, hr_name, hr_email FROM recruiters WHERE company_status='accepted'";
           $data = mysqli_query($dbc, $query);
           if(mysqli_num_rows($data) != 0){
         ?>
@@ -52,6 +92,8 @@
                         '<td>' . $row["company_category"] . '</td>' .
                         '<td>' . $row["hr_name"] . '</td>' .
                         '<td>' . $row["hr_email"] . '</td>' .
+                        '<td><form action="' . $_SERVER['PHP_SELF'] . '?id=' . $row["company_id"] . '&tab=1" method="post">' .
+                        '<button type="reject" class="btn btn-danger" name="reject">Reject</button></form></td>' .
                     '</tr>';
               $curr = $curr + 1;
             }
@@ -64,7 +106,7 @@
         <?php } ?>
       </table>
     </div>
-    <div class="tab-pane fade" id="pending" role="tabpanel">
+    <div class="tab-pane fade <?php if($activeTab==2){echo 'show active';} ?>" id="pending" role="tabpanel">
       <table class="table">
         <thead class="thead-light">
           <tr>
@@ -73,11 +115,12 @@
             <th scope="col">Category</th>
             <th scope="col">HR Name</th>
             <th scope="col">Email</th>
+            <th scope="col">Action</th>
           </tr>
         </thead>
         <?php
           $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-          $query = "SELECT company_name, company_category, hr_name, hr_email FROM recruiters WHERE company_status='pending'";
+          $query = "SELECT company_id, company_name, company_category, hr_name, hr_email FROM recruiters WHERE company_status='pending'";
           $data = mysqli_query($dbc, $query);
           if(mysqli_num_rows($data) != 0){
         ?>
@@ -90,6 +133,9 @@
                         '<td>' . $row["company_category"] . '</td>' .
                         '<td>' . $row["hr_name"] . '</td>' .
                         '<td>' . $row["hr_email"] . '</td>' .
+                        '<td><form action="' . $_SERVER['PHP_SELF'] . '?id=' . $row["company_id"] . '&tab=2" method="post">' . 
+                        '<button type="approve" class="btn btn-success" name="approve">Approve</button> ' .
+                        '<button type="reject" class="btn btn-danger" name="reject">Reject</button></form></td>' .
                     '</tr>';
               $curr = $curr + 1;
             }
@@ -102,7 +148,7 @@
         <?php } ?>
       </table>
     </div>
-    <div class="tab-pane fade" id="rejected" role="tabpanel">
+    <div class="tab-pane fade <?php if($activeTab==3){echo 'show active';} ?>" id="rejected" role="tabpanel">
       <table class="table">
         <thead class="thead-light">
           <tr>
@@ -111,11 +157,12 @@
             <th scope="col">Category</th>
             <th scope="col">HR Name</th>
             <th scope="col">Email</th>
+            <th scope="col">Action</th>
           </tr>
         </thead>
         <?php
           $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-          $query = "SELECT company_name, company_category, hr_name, hr_email FROM recruiters WHERE company_status='rejected'";
+          $query = "SELECT company_id, company_name, company_category, hr_name, hr_email FROM recruiters WHERE company_status='rejected'";
           $data = mysqli_query($dbc, $query);
           if(mysqli_num_rows($data) != 0){
         ?>
@@ -128,6 +175,8 @@
                         '<td>' . $row["company_category"] . '</td>' .
                         '<td>' . $row["hr_name"] . '</td>' .
                         '<td>' . $row["hr_email"] . '</td>' .
+                        '<td><form action="' . $_SERVER['PHP_SELF'] . '?id=' . $row["company_id"] . '&tab=3" method="post">' . 
+                        '<button type="approve" class="btn btn-success" name="approve">Approve</button></form></td>' .
                     '</tr>';
               $curr = $curr + 1;
             }
