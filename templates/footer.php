116385f439e22ae5b7277a65b106d61899281a52
Harsh <29hj29@gmail.com>
2019-02-10 15:36:55 +0530

    Auto Close Alert in 5 seconds

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,10 @@
   <!-- jQuery, Popper.js, Bootstrap JS -->
-  <script src="/TPC-management-app/scripts/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="/TPC-management-app/scripts/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <script src="/TPC-management-app/scripts/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
   <script src="/TPC-management-app/scripts/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
   <script src="/TPC-management-app/scripts/studentProfile.js"></script>
   <script src="/TPC-management-app/scripts/studentLogin.js"></script>
   <script src="/TPC-management-app/scripts/createPosition.js"></script>
+  <script src="/TPC-management-app/scripts/index.js"></script>
 </body>
 </html>
