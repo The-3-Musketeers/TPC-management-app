6381f3a0abf7cc1cfa70715001f5c43e155b78bc
PrP-11 <prp9812@gmail.com>
2019-08-24 18:49:00 +0530

    Add footer

---
+++ scripts/footer.js
@@ -0,0 +1,32 @@
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+}
+  
+var arr = [["Praduman", "PrP-11"], ["Harsh", "harsh-jindal"], ["Divyanshu", "DNS-404"]];
+var github = "https://github.com/";
+arr = shuffle(arr);
+
+function setValus(arr) {
+    document.getElementById("link1").href=github+arr[0][1];
+    document.getElementById("link2").href=github+arr[1][1];
+    document.getElementById("link3").href=github+arr[2][1];
+    document.getElementById("dev1").innerHTML=arr[0][0] + ",";
+    document.getElementById("dev2").innerHTML=arr[1][0] + ",";
+    document.getElementById("dev3").innerHTML=arr[2][0];
+};
+setValus(arr);
--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,14 @@
+  <!-- Footer -->
+  <footer class="footer" class="page-footer font-small blue" style="background-color: #e3f2fd;">
+    <div class="container">
+      <span class="text-muted">Developed by: </span>
+      <a href="#" id="link1"><span id="dev1">1</span></a>
+      <a href="#" id="link2"><span id="dev2">2</span></a>
+      <a href="#" id="link3"><span id="dev3">3</span></a>
+    </div>
+  </footer>
+  <script src="/TPC-management-app/scripts/footer.js"></script>
+  
   <!-- jQuery, Popper.js, Bootstrap JS, Chart.js -->
   <script src="/TPC-management-app/scripts/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <script src="/TPC-management-app/scripts/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
