9a664055bf1c27cd7bea4152eb3b6e648a91216a
PrP-11 <prp9812@gmail.com>
2019-08-25 01:43:16 +0530

    Add footer

--- student/login.php
+++ student/login.php
@@ -139,5 +139,14 @@
         header('Location: ' . $home_url);
       }
     ?>
-
+<!-- Footer -->
+<footer class="footer" class="page-footer font-small blue" style="background-color: #e3f2fd;">
+    <div class="container">
+        <span class="text-muted">Developed by: </span>
+        <a href="#" id="link1"><span id="dev1">1</span></a>
+        <a href="#" id="link2"><span id="dev2">2</span></a>
+        <a href="#" id="link3"><span id="dev3">3</span></a>
+    </div>
+</footer>
+<!-- Footer -->
 <?php require_once('../templates/footer.php');?>
