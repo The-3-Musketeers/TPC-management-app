0271839d05e33d56eecf115868b3fe79fc848c90
PrP-11 <prp9812@gmail.com>
2019-11-14 13:29:10 +0530

    update stats query and add dummy data

--- admin/stats/placementStats.php
+++ admin/stats/placementStats.php
@@ -13,12 +13,12 @@ $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 // fetch placement stats
 $placedQuery = "SELECT D.degree_name AS degree_name, B.branch_name AS branch_name, "
 			."placed_table.placed AS placed, remaining_table.remaining AS remaining "
-			."FROM (SELECT db_id, 0 AS placed FROM degree_branch WHERE db_id NOT IN (SELECT db_id FROM students_data WHERE job_offers!='') "
+			."FROM (SELECT db_id, 0 AS placed FROM degree_branch WHERE db_id NOT IN (SELECT db_id FROM students_data WHERE final_accepted_offer!='') "
 			."UNION "
-			."SELECT db_id, COUNT(roll_number) AS placed FROM students_data WHERE job_offers!='' GROUP BY db_id) AS placed_table, "
-			."(SELECT db_id, 0 AS remaining FROM degree_branch WHERE db_id NOT IN (SELECT db_id FROM students_data WHERE job_offers IS NULL OR job_offers='') "
+			."SELECT db_id, COUNT(roll_number) AS placed FROM students_data WHERE final_accepted_offer!='' GROUP BY db_id) AS placed_table, "
+			."(SELECT db_id, 0 AS remaining FROM degree_branch WHERE db_id NOT IN (SELECT db_id FROM students_data WHERE final_accepted_offer IS NULL OR final_accepted_offer='') "
 			."UNION "
-			."SELECT db_id, COUNT(roll_number) AS remaining FROM students_data WHERE job_offers IS NULL OR job_offers='' GROUP BY db_id) AS remaining_table, "
+			."SELECT db_id, COUNT(roll_number) AS remaining FROM students_data WHERE final_accepted_offer IS NULL OR final_accepted_offer='' GROUP BY db_id) AS remaining_table, "
 			."degree AS D, branch AS B, degree_branch AS DB "
 			."WHERE placed_table.db_id = remaining_table.db_id AND placed_table.db_id = DB.db_id "
 			."AND D.degree_id = DB.degree_id AND B.branch_id = DB.branch_id";
